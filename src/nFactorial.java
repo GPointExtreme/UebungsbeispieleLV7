64b335f327520a717c6322c4cccad33f2cb6c049
GPointExtreme <dominik.geiger@hotmail.com>
2016-01-13 21:30:58 +0100

    LuckyNumber finden mit 8.

--- src/nFactorial.java
+++ src/nFactorial.java
@@ -4,6 +4,7 @@ public class nFactorial {
 	public static void main(String[] args) {
 		
 		System.out.println(fac(5));
+		System.out.println(isLuckyNumber(23789));
 		
 	}
 	
@@ -12,5 +13,25 @@ public class nFactorial {
 			return 1;
 		}
 		return fac(n-1)*n;
+	}
+	
+	public static boolean isLuckyNumber(int n) {
+		if(n==0) {
+			return false;
+		}
+		else if((n-8)%10==0) {
+			return true;
+		}
+		return isLuckyNumber(n/10);
+	}
+	
+	public static boolean isLuckyNumber2(int n) {
+		if(n==0) {
+			return false;
+		}
+		else if(n%10==8) {
+			return true;
+		}
+		return isLuckyNumber2(n/10);
 	}
 }
